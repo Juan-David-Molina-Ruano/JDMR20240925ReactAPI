7206c4d27a3f66fd79b3d9e9d3713ef15cf1a3f2
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-26 20:09:16 -0600

    Refactorizar URL de API para utilizar el dominio de producción y cambiar la URL local de desarrollo a HTTPS

--- constants/Servicio.js
+++ constants/Servicio.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const API_URL = 'https://davidruano.bsite.net/api';
-const API_URL_LOCAL = 'http://localhost:7132';
+const API_URL_LOCAL = 'https://localhost:7132';
 
 const servicio = axios.create({
-    baseURL: API_URL,
+    baseURL: API_URL_LOCAL,
     headers: {
         'Content-Type': 'application/json',
     },
